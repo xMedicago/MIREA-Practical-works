c37de2df87c1d80fdf9459dcbdf62a34b0b2842b
Alexander <87269586+xMedicago@users.noreply.github.com>
2022-10-05 11:12:20 +0300

    Update Практическая_работа_3_Разветвляющийся_алгоритм.c

--- Практическая_работа_3_Разветвляющийся_алгоритм.c
+++ Практическая_работа_3_Разветвляющийся_алгоритм.c
@@ -18,9 +18,17 @@ int main() {
     printf("Введите z: ");
     scanf_s("%lf", &z);
 
-    if ((fabs(x - y) * z + pow(x, 2) != 0) && (x >= -1 && x <= 1)) {
-        double result = 5 * atan(x) - 1. / 4. * acos(x) * ((x + 3 * fabs(x - y) + pow(x, 2)) / (fabs(x - y) * z + pow(x, 2)));
-        printf("%.3lf", result);
+    if ((fabs(x - y) * z + pow(x, 2) != 0)) {
+        if (x >= -1) {
+            if (x <= 1) {
+                double result = 5 * atan(x) - 1. / 4. * acos(x) * ((x + 3 * fabs(x - y) + pow(x, 2)) / (fabs(x - y) * z + pow(x, 2)));
+                printf("%.3lf", result);
+            }
+            else
+                printf("ERROR!\n");
+        }
+        else
+            printf("ERROR!\n");
     }
     else
         printf("ERROR!\n");
